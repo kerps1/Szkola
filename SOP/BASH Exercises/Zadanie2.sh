4b5079d55e0b7f4159ed8491249299cb2cb59cd4
seba <sebasolew@gmail.com>
2017-05-05 18:16:13 +0200

    Zadanie2 fixes

--- SOP/BASH Exercises/Zadanie2.sh
+++ SOP/BASH Exercises/Zadanie2.sh
@@ -1,5 +1,9 @@
-echo "<html><body><table><thead><tr><th>login</th><th>home</th><th>shell</th>
-</t$></thead><tbody>"
-cat /etc/passwd|awk -F':' '{print "<tr><td>"  $1 "</td><td>" $6 "</td><td>" 
-$7 "</td></tr>}' 
-echo "</tbody></table></body></html>"
+HTML="<table><thead><tr><th>login</th><th>home</th><th>shell</th>
+</tr></thead><tbody>"
+
+HTML+=`cat /etc/passwd | awk -F':' '{print "<tr><td>" $1 "</td><td>" $6 "</td><td>" 
+$7 "</td></tr>"}'`
+
+HTML+="</tbody></table>"
+
+echo $HTML > index.html
