cda2e6cf58f1d61cd3f576411e4bb5f09b97c465
seba <sebasolew@gmail.com>
2017-06-04 00:56:35 +0200

    estetic fix

--- SOP/zboj/zad_1.sh
+++ SOP/zboj/zad_1.sh
@@ -2,8 +2,8 @@
 
 if [ -z $1 ] 
 then
-        echo "Podaj ścieżkę"
+        echo "Podaj sciezke do katalogu ktory chcesz obserwowac"
         exit 1
 fi
 
-inotifywait -m $1 -e modify,create,delete,move
+inotifywait -m -e modify,create,delete,move $1
