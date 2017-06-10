d7239c05fe918e85f4a1466dfcc29bc70d63d328
seba <sebasolew@gmail.com>
2017-06-10 18:37:19 +0200

    widelec #3 maly fix

--- SOP/CPP/fork/3.cpp
+++ SOP/CPP/fork/3.cpp
@@ -4,7 +4,7 @@
 int main()
 {
     pid_t pid = fork();
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < 99; i++) {
         if (pid == 0) {
             pid = fork();
             continue;
